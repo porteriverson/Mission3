8da7db38d7ab3520904eb58e50c4df54c45f64f7
porteriverson <live4soccer02@icloud.com>
2025-01-24 18:01:45 -0700

    Final commit, everything runs correctly on my local.

--- FoodItem.cs
+++ FoodItem.cs
@@ -1,6 +1,77 @@
+using System.Runtime.InteropServices.JavaScript;
+using System.Linq;
+
 namespace Mission3;
 
 public class FoodItem
 {
+    public string Name {get; set;}
+    public string Category {get; set;}
+    public int Quantity {get; set;}
+    public DateTime Date {get; set;}
     
+    public FoodItem(string name, string category, int quantity, DateTime xDate)
+    // Constructor for the FoodItem Objects, gives them their attributes
+    {
+        Name = name;
+        Category = category;
+        Quantity = quantity;
+        Date = xDate;
+    }
+
+    public static FoodItem addItem()
+    {
+        // Get all user input and create a new FoodItem object
+        Console.WriteLine("Answer the questions to add new item");
+        Console.WriteLine("What is the name of the food item?");
+        string name = Console.ReadLine();
+        Console.WriteLine("In what category is the food item?");
+        string category = "";
+        category = Console.ReadLine();
+        Console.WriteLine("What is the quantity of the food item?");
+        string sQuant = Console.ReadLine();
+        int quantity = int.Parse(sQuant);
+        Console.WriteLine("When does this item expire? (MM/DD/YYYY)");
+        string sDate = Console.ReadLine();
+        DateTime xDate = DateTime.Parse(sDate);
+        
+        FoodItem food = new FoodItem(name, category, quantity, xDate);
+        
+        return food;
+    }
+
+    public static List<FoodItem> PrintFoodItems(List<FoodItem> foodItems)
+    {
+        // sort the food so the soonest expiring comes first
+        var orderedFoodList = foodItems.OrderBy(item => item.Date).ToList();
+        DateTime currentDate = DateTime.Now;
+        Console.WriteLine("Current Inventory (Soonest expiring listed first):");
+        // loop through the list and print out the soonest expiring foods
+        for (int i = 0; i < orderedFoodList.Count; i++)
+        {
+            int daysRemaining = orderedFoodList[i].Date.Subtract(currentDate).Days;
+            // if the food is expired, tell them how long ago. 
+            if (daysRemaining < 0)
+            {
+                Console.WriteLine(
+                    $"{i + 1} - Expired {Math.Abs(daysRemaining)} Days ago on {orderedFoodList[i].Date.ToShortDateString()} " +
+                    $"- {orderedFoodList[i].Name}: {orderedFoodList[i].Quantity}");
+            }
+            // tell the user how many days until it expires. 
+            else if (daysRemaining > 0)
+            {
+                Console.WriteLine(
+                    $"{i + 1} - Expires in {daysRemaining} Days on {orderedFoodList[i].Date.ToShortDateString()} " +
+                    $"- {orderedFoodList[i].Name}: {orderedFoodList[i].Quantity}");
+            }
+            // if daysRemaining is zero, item expires today
+            else
+            {
+                Console.WriteLine(
+                    $"{i + 1} - Expires today, {orderedFoodList[i].Date.ToShortDateString()} " +
+                    $"- {orderedFoodList[i].Name}: {orderedFoodList[i].Quantity}");
+            }
+        }
+        return orderedFoodList;
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,72 @@
-﻿
+﻿using Mission3;
 
-Console.WriteLine("Welcome to your food inventory!");
+Console.WriteLine("Welcome to your food bank system!");
+// Create the global list of food items to store in the bank
+List<FoodItem> foodItems = new List<FoodItem>();
+Main();
+
+
+void Main()
+{
+    Console.WriteLine("How would you like to proceed? \n " +
+                      "1 - Add a Food Item \n " +
+                      "2 - Delete a Food Item \n " +
+                      "3 - Print list of all current items \n " +
+                      "4 - Exit \n");
+    Console.WriteLine("Enter a number: ");
+
+    
+    // get the number that corresponds with the action the user wants
+    string sInput = Console.ReadLine();
+    //try to parse into an integer and if not, give an error message
+    bool result = int.TryParse(sInput, out int determiner);
+    if (result)
+    {
+        // Add a food item
+        if (determiner == 1)
+        {
+            FoodItem item = FoodItem.addItem(); // create the food item using the addItem method
+            foodItems.Add(item); // add that food item to the global list
+            Console.WriteLine($"Successfully added {item.Quantity} {item.Name} to your {item.Category} inventory \n");
+            Main();
+
+        }
+        // Delete a food item
+        else if (determiner == 2)
+        {
+            // Print the list so the user can select from there.  
+            List<FoodItem> orderedList = FoodItem.PrintFoodItems(foodItems);
+            Console.WriteLine("Which food item would you like to delete? (Enter the number)");
+            // Get the input from the user about which item to delete
+            string sDelete = Console.ReadLine();
+            int intDelete = int.Parse(sDelete);
+            // save the item in a temporary variable to be able to print the successful deletion message.
+            FoodItem deletedFood = orderedList[intDelete - 1]; 
+            // Remove the item from the global list
+            orderedList.Remove(orderedList[intDelete - 1]);
+            Console.WriteLine($"Successfully deleted {deletedFood.Quantity} {deletedFood.Name} from you inventory\n");
+            // Print again so user can see current inventory
+            FoodItem.PrintFoodItems(orderedList);
+            Main();
+
+
+        }
+        // Print list of current items
+        else if (determiner == 3)
+        {
+            FoodItem.PrintFoodItems(foodItems);
+            Main();
+        }
+        // Goodbye message
+        else if (determiner == 4)
+        {
+            Console.WriteLine("Thank you using your food bank system! Goodbye!");
+        }
+    }
+    // Error message if input is invalid
+    else
+    {
+        Console.WriteLine("Invalid input, please try again.\n");
+        Main();
+    }
+}
